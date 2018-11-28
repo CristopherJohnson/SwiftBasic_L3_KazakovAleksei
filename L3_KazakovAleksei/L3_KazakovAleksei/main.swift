ecd6cd939f404a6b3302048e75f65ec95336964a
CristopherJohnson <henkok127@mail.ru>
2018-11-28 19:47:01 +0300

    start

---
+++ L3_KazakovAleksei/L3_KazakovAleksei/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L3_KazakovAleksei
+//
+//  Created by Алексей Казаков on 28/11/2018.
+//  Copyright © 2018 Алексей Казаков. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
